0a4d651bd0079518152ac32a4f7845ee33653a5d
Тараскин Михаил <MITaraskin@domclick.ru>
2021-01-05 14:29:33 +0300

    Yandex metric, google anlytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,12 +50,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `G-167BRK3KBL`,
-      },
-    },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
